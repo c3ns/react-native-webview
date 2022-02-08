1ccb85f8335040dcaf6621c0ed1a639fbfc99058
Ernestas Radzvilas <36697480+c3ns@users.noreply.github.com>
2022-02-08 12:06:21 +0200

    Update RNCWebViewManager.m

--- apple/RNCWebViewManager.m
+++ apple/RNCWebViewManager.m
@@ -251,7 +251,7 @@ shouldStartLoadForRequest:(NSMutableDictionary<NSString *, id> *)request
   callback(request);
 
   // Block the main thread for a maximum of 250ms until the JS thread returns
-  if ([_shouldStartLoadLock lockWhenCondition:0 beforeDate:[NSDate dateWithTimeIntervalSinceNow:0]]) {
+  if ([_shouldStartLoadLock lockWhenCondition:0 beforeDate:[NSDate dateWithTimeIntervalSinceNow:.01]]) {
     BOOL returnValue = _shouldStartLoad;
     [_shouldStartLoadLock unlock];
     _shouldStartLoadLock = nil;
